d38f15c9e0940c0443e6f61f24419d1503cc4766
stian.larsen <stian.larsen@sparebank1.no>
2025-12-17 12:41:41 +0100

    Bumping rampkit

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  reactCompiler: true,
   experimental: {
-    reactCompiler: true,
     cssChunking: true,
   },
 };
